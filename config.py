d949d8ba4efe2aedd64a058532e562715735c3b2
javaernesto <javaernesto@github.com>
2021-03-16 16:14:40 +0100

    add params for elections

---
+++ config.py
@@ -0,0 +1,12 @@
+# Set elections parameters
+N_VOTERS = 10
+N_CHOICE = 5
+
+# Set public parameters (we must have p prime and p > N_VOTERS)
+P = 17 
+
+# Set ports for voters and verifiers
+#VERIFS = [
+#    ("localhost", 8764),
+#    ("localhost", 8763)]
+#VOTERS = [("localhost", 8762 - i) for i in range(N_VOTERS)]
