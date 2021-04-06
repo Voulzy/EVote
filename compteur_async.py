f04d2704ec36057ab71390abed6d1653a14fc9a4
Voulzy <aguer.victor@gmail.com>
2021-04-06 21:40:26 +0200

    added check on public key

--- compteur_async.py
+++ compteur_async.py
@@ -3,17 +3,40 @@ import ssl
 import protocol
 from config import N_CHOICE, P, port_compteur_1_c, port_compteur_1_v
 import asyncio
+from asn1crypto import pem, x509
 import pickle
+from OpenSSL import crypto
+import sys
 import time
+import os
 crtfile ='Voteur_0.cert'
 key_file='Voteur_0.key'
+nbVotants=int(sys.argv[1])
 cafile= 'myCA.cert'
 data=[0,0,0,0,0,0,0,0,0,0]
 vote=[]
+pubkeys = []
+didvote = []
 async def handle_client(reader,writer,data1):
 	global data
-	print("Handle 1")	
-	while True : 
+
+	print("Handle 1")
+	while True :
+		pub= writer.get_extra_info('ssl_object')
+		der= pub.getpeercert(binary_form=True)
+		crtObj = crypto.load_certificate(crypto.FILETYPE_ASN1, der)
+		pubKeyObject = crtObj.get_pubkey()
+		pubKeyString = crypto.dump_publickey(crypto.FILETYPE_PEM, pubKeyObject)
+		if pubKeyString in pubkeys:
+			if pubKeyString in didvote:
+				print("Already voted")
+				break
+			else:
+				print("Allowed")
+				didvote.append(pubKeyString)
+		else:
+			print("Not allowed")
+			break
 		try : 
 			size_bytes = await reader.readexactly(4)
 			if not size_bytes:
@@ -89,7 +112,20 @@ def compteur_exchange(port,context,vecteur):
 	time.sleep(1)
 	conn.close()
 	return data
+
+def get_public_keys(i):
+	for j in range(3,i+1):
+		file_path = os.path.join(os.getcwd(),'Voteur_{}.cert').format(j-1)
+		f = open(file_path,'r')
+		cert = f.read()
+		crtObj = crypto.load_certificate(crypto.FILETYPE_PEM, cert)
+		pubKeyObject = crtObj.get_pubkey()
+		pubKeyString = crypto.dump_publickey(crypto.FILETYPE_PEM,pubKeyObject)
+		pubkeys.append(pubKeyString)
+
 async def main():
+	get_public_keys(nbVotants)
+	print(pubkeys)
 	context=ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
 	context.load_cert_chain(crtfile,keyfile=key_file)
 	context.load_verify_locations(cafile=cafile)
